ad38bbd5a7f51fe8e53f3f6c0d27930d01b780f3
Carla Barruffaldi <cbarruffaldi@itba.edu.ar>
2021-10-22 15:23:52 +0200

    Order route

--- routes/users.js
+++ routes/users.js
@@ -53,7 +53,7 @@ router.get("/", verifyTokenAndAdmin, async (req, res) => {
 //GET USER STATS
 router.get("/stats", verifyTokenAndAdmin, async (req, res) => {
     const date = new Date();
-    const lastYear = new Date(date.setFullYear(date.getFullYear - 1));
+    const lastYear = new Date(date.setFullYear(date.getFullYear() - 1));
 
     try {
         const data = await User.aggregate([
