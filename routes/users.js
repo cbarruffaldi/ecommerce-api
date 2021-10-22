84ad20cb969243588db683e8bf90ce6d2d9d3882
Carla Barruffaldi <cbarruffaldi@itba.edu.ar>
2021-10-22 14:24:23 +0200

    Delete user method

--- routes/users.js
+++ routes/users.js
@@ -16,6 +16,16 @@ router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
     } catch (err) {
         res.status(500).json(err);
     }
-})
+});
+
+//DELETE
+router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
+    try {
+        await User.findByIdAndDelete(req.params.id);
+        res.status(200).json("User has been deleted");
+    } catch(err) {
+        res.status(500).json(err);
+    }
+});
 
 module.exports = router;
