8a8c9d390b1e844e2894f841d28f4840b4a5f1cb
Carla Barruffaldi <cbarruffaldi@itba.edu.ar>
2021-10-26 12:54:30 +0200

    fix update product

--- routes/product.js
+++ routes/product.js
@@ -18,7 +18,7 @@ router.post("/", verifyTokenAndAdmin, async (req, res) => {
 //UPDATE
 router.put("/:id", verifyTokenAndAdmin, async (req, res) => {
     try {
-        const updatedProduct = await Product.findByIdAndUpdate(req.params.id, {
+        const updatedProduct = await Product.findByIdAndUpdate(req.params._id, {
             $set: req.body
         }, {new: true});
         res.status(200).json(updatedProduct);
